1afadc808f6b8cab7ea3d04cc7cf25c2d3e5eaab
KSVarun <ksvarun001@gmail.com>
2021-07-17 16:28:04 +0530

    adding links to chrome storage conditional rendering of add button

--- content.js
+++ content.js
@@ -1,25 +1,22 @@
 chrome.runtime.sendMessage({ todo: 'showPageAction' });
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if ((request.todo = 'clickJoinNow')) {
-    if (document.querySelector('div[jsname=Qx7uuf]')) {
-      const joinNowButton = document.querySelector('div[jsname=Qx7uuf]');
-      joinNowButton.click();
-    }
-  }
-  if ((request.todo = 'mute&stopVideo')) {
-    let mutedOnce = false;
-    const data = JSON.parse(localStorage.getItem('muted'));
-    if (data) {
-      mutedOnce = data.mutedOnce;
-    }
-    const muteButton = document.querySelector('div[jscontroller=lCGUBd]');
 
-    if (muteButton) {
-      const isMuted = muteButton.getAttribute('data-is-muted');
-      if (isMuted === 'false' && !data && !mutedOnce) {
-        muteButton.click();
-        localStorage.setItem('muted', JSON.stringify({ mutedOnce: true }));
+chrome.storage.sync.set({ link: window.location.href });
+
+chrome.storage.sync.get('links', (data) => {
+  if (data.links.includes(window.location.href)) {
+    setTimeout(() => {
+      if (document.querySelector('div[jsname=Qx7uuf]')) {
+        const joinNowButton = document.querySelector('div[jsname=Qx7uuf]');
+        joinNowButton.click();
       }
-    }
+
+      const muteButton = document.querySelector('div[jscontroller=lCGUBd]');
+      if (muteButton) {
+        const isMuted = muteButton.getAttribute('data-is-muted');
+        if (isMuted === 'false') {
+          muteButton.click();
+        }
+      }
+    }, 4000);
   }
 });
--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+function promisifiedGetLink() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get('link', (data) => {
+      resolve(data.link);
+    });
+  });
+}
 
+function promisifiedGetLinks() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get('links', (data) => {
+      resolve(data.links);
+    });
+  });
+}
+
+async function getLink() {
+  return await promisifiedGetLink();
+}
+
+async function getAllLinks() {
+  return await promisifiedGetLinks();
+}
+
+function addLink(link, links) {
+  if (
+    links.length &&
+    !links.includes(link) &&
+    link !== 'https://meet.google.com/'
+  ) {
+    const updatedLinks = [...links, link];
+    chrome.storage.sync.set({ links: updatedLinks });
+  } else {
+    const newLinks = [link];
+    chrome.storage.sync.set({ links: newLinks });
+  }
+}
+
+async function init() {
+  const link = await getLink();
+  const links = await getAllLinks();
+  const linkContainer = document.querySelector('#link');
+  const addButton = document.querySelector('#add');
+  linkContainer.innerHTML = link;
+  if (!links.length || !links.includes(link)) {
+    addButton.style.opacity = '1';
+  }
+
+  addButton.addEventListener('click', () => addLink(link, links));
+}
+
+init();
